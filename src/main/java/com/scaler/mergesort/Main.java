12f5375d6f18ac6c92c2da17a0747203cf9a925f
Naman Bhalla <namanbhalla1998@gmail.com>
2023-09-04 08:50:01 +0530

    End of class 2

---
+++ src/main/java/com/scaler/mergesort/Main.java
@@ -0,0 +1,29 @@
+package com.scaler.mergesort;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        List<Integer> arrayToSort = List.of(
+                8, 2, 4, 1, 9, 6, 0, 7
+        );
+
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        Sorter sorter = new Sorter(arrayToSort, es);
+
+        List<Integer> sortedArray = sorter.call();
+
+        for (Integer in: sortedArray) {
+            System.out.println(in);
+        }
+
+//        Thread t = new Thread();
+//        t.stop();;
+
+//        es.shutdown();
+    }
+}
---
+++ src/main/java/com/scaler/mergesort/Sorter.java
@@ -0,0 +1,75 @@
+package com.scaler.mergesort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Sorter implements Callable<List<Integer>> {
+    private List<Integer> arrayToSort;
+    private ExecutorService es;
+
+    public Sorter(List<Integer> arrayToSort, ExecutorService es) {
+        this.arrayToSort = arrayToSort;
+        this.es = es;
+    }
+
+    @Override
+    public List<Integer> call() throws Exception {
+        if (arrayToSort.size() <= 1) {
+            return arrayToSort;
+        }
+
+        List<Integer> leftArrayToSort = new ArrayList<>();
+        List<Integer> rightArrayToSort = new ArrayList<>();
+
+        int mid = arrayToSort.size()/ 2;
+
+        for (int i = 0; i < mid; ++i) {
+            leftArrayToSort.add(arrayToSort.get(i));
+        }
+
+        for (int i = mid; i < arrayToSort.size(); ++i) {
+            rightArrayToSort.add(arrayToSort.get(i));
+        }
+
+
+        Sorter leftArraySorter = new Sorter(leftArrayToSort, es);
+        Future<List<Integer>> sortedLeftArrayFuture = es.submit(leftArraySorter);
+
+        Sorter rightArraySorter = new Sorter(rightArrayToSort, es);
+        Future<List<Integer>> sortedRightArrayFuture = es.submit(rightArraySorter);
+
+        List<Integer> sortedArray = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+
+        List<Integer> sortedLeftArray = sortedLeftArrayFuture.get(); // blocking
+        List<Integer> sortedRightArray = sortedRightArrayFuture.get();
+
+        while (i < sortedLeftArray.size() && j < sortedRightArray.size()) {
+            if (sortedLeftArray.get(i) <= sortedRightArray.get(j)) {
+                sortedArray.add(sortedLeftArray.get(i));
+                i++;
+            } else {
+                sortedArray.add(sortedRightArray.get(j));
+                j++;
+            }
+        }
+
+        while (i < sortedLeftArray.size()) {
+            sortedArray.add(sortedLeftArray.get(i));
+            i++;
+        }
+
+        while (j < sortedRightArray.size()) {
+            sortedArray.add(sortedRightArray.get(j));
+            j++;
+        }
+
+        return sortedArray;
+    }
+}
